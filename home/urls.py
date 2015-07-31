a952120052eac6c8d35dcaa113b4c02db34ebc2f
Cesar Acosta <cacosta333@hotmail.com>
2015-07-31 13:28:33 -0400

    Changes made to layout of the site. Added Contact page with appropriate fields

--- home/urls.py
+++ home/urls.py
@@ -6,8 +6,13 @@ urlpatterns = [
 
 	url(r'^$', views.index, name='index'),
 
-	#ex /about_me
+	#ex: /about_me
 	url(r'^about_me/$', views.about_me, name='about_me'),
 
+	#ex: /resume
+	url(r'^resume/$', views.resume, name='resume'),
+
+	#ex: /contact
+	url(r'^contact/$', views.contact, name='contact'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
 
-
+from .form import ContactForm
 #Create your views here
 
 def index(request):
@@ -11,3 +13,25 @@ def about_me(request):
 
 def bad_request(request):
 	return render(request, "home/error_pages/404.html")
+
+def resume(request):
+	with open('home/static/home/images/CesarAcostaResume.pdf', 'r') as pdf:
+		response = HttpResponse(pdf.read(), content_type='application/pdf')
+		response['Content-Disposition'] = 'filename=cesar_acosta_resume.pdf'
+		return response
+	pdf.closed
+
+def contact(request):	
+	# If this is a POST request we need to process the form data
+	if request.method == 'POST':
+		#Create a form instance and populate it with data from the request:
+		form = ContactForm(request.POST)
+		# check whether it's valid:
+		if form.is_valid():
+			message = form.cleaned_data['message']
+			sender = form.cleaned_data['sender']
+			return HttpResponseRedirect('home:index')
+	else:
+		form = ContactForm()
+
+	return render(request, 'home/contact.html', {'form': form})
